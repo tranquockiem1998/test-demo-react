2fa2ff4b9ad7c49a2a8f26847c57c6f873461312
Ngo Chau <chau78840@gmail.com>
2024-08-18 14:38:42 +0700

    video 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,17 @@
+// Class component
+// Function component
+import React from "react";
+
+class MyComponent extends React.Component {
+  // JSX
+  render() {
+    return (
+      <div>
+        My first component
+        {Math.random()}
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
