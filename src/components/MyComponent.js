c898935332a462608ce2654f47d6e637219878cf
Ngo Chau <chau78840@gmail.com>
2024-08-19 14:56:58 +0700

    video 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,5 +1,6 @@
 // Class component
 // Function component
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
 import React from "react";
 
 class MyComponent extends React.Component {
@@ -9,11 +10,21 @@ class MyComponent extends React.Component {
     age: 26,
   };
 
+  handleClick = (event) => {
+    console.log(">> Click me my button");
+    console.log("My name is", this.state.name);
+  };
+
+  handleOnMouseOver(event) {
+    console.log(event.pageX);
+  }
   // JSX
   render() {
     return (
       <div>
         My name is {this.state.name} and I'm from {this.state.address}
+        <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+        <button onClick={this.handleClick}>Click me</button>
       </div>
     );
   }
