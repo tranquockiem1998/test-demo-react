4e2ed0d4d210125ce3534929e33a87a2b58e60cc
Ngo Chau <chau78840@gmail.com>
2024-08-22 14:58:01 +0700

    video 24

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,14 +2,28 @@ import React from "react";
 
 class DisplayInfor extends React.Component {
   render() {
-    console.log(this.props);
     // destructuring array/object
-    const { name, age } = this.props;
+    const { listUsers } = this.props;
     // props => viết tắt của từ properties
     return (
       <div>
+        {listUsers.map((user) => {
+          return (
+            <div key={user.id}>
+              <div>My name is {user.name}</div>
+              <div>My age is {user.age}</div>
+              <hr></hr>
+            </div>
+          );
+        })}
+        {/* {<div>My name is {name}</div>
+        <div>My age is {age}</div>
+        <hr></hr>
         <div>My name is {name}</div>
         <div>My age is {age}</div>
+        <hr></hr>
+        <div>My name is {name}</div>
+        <div>My age is {age}</div>} */}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,16 +6,26 @@ import UserInfor from "./UserInfor";
 import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
+  state = {
+    listUser: [
+      { id: 1, name: "Hoi Dan IT", age: 30 },
+      { id: 2, name: "Eric", age: 26 },
+      { id: 3, name: "HarryPhamDev", age: 27 },
+    ],
+  };
+
   // JSX
+  // DRY: don't repeat yourself
   render() {
     const myInfor = ["ab", "c", "c"];
     return (
       <div>
         <UserInfor />
         <br /> <br />
-        <DisplayInfor name="Hoi Dan IT" age="30" />
-        <hr></hr>
-        <DisplayInfor name="Eric" age={26} myInfor={myInfor} />
+        <DisplayInfor
+          listUsers={this.state.listUser}
+          users={this.state.listUser}
+        />
       </div>
     );
   }
