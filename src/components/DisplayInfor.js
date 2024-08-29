6d6c42e5ec3af67aefb0882e3c85db93cccce380
Ngo Chau <chau78840@gmail.com>
2024-08-29 14:07:58 +0700

    video 33

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -3,14 +3,36 @@ import "./DisplayInfor.scss";
 import logo from "./../logo.svg";
 
 class DisplayInfor extends React.Component {
-  state = {
-    isShowListUser: true,
-  };
+  constructor(props) {
+    console.log(">>> call me constructor: 1");
+    super(props);
+    //babel compiler
+    this.state = {
+      isShowListUser: true,
+    };
+  }
+
+  componentDidMount() {
+    console.log(">>> call me component did mount");
+    setTimeout(() => {
+      document.title = "Eric & Hoi Dan IT";
+    }, 3000);
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log(">>> call me component did update", this.props, prevProps);
+    if (this.props.listUsers !== prevProps.listUsers) {
+      if (this.props.listUsers.length === 5) {
+        alert("You have 5 users!");
+      }
+    }
+  }
 
   handleShowHide = function () {
     this.setState({ isShowListUser: !this.state.isShowListUser });
   };
   render() {
+    console.log("call me render");
     // destructuring array/object
     const { listUsers } = this.props;
     // props => viết tắt của từ properties
