6e2dcb44c420a3b04ae0f1db811c57bcc151ea2f
Ngo Chau <chau78840@gmail.com>
2024-08-30 14:45:49 +0700

    video 35

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,77 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "./../logo.svg";
 
-class DisplayInfor extends React.Component {
-  constructor(props) {
-    console.log(">>> call me constructor: 1");
-    super(props);
-    //babel compiler
-    this.state = {
-      isShowListUser: true,
-    };
-  }
+//class DisplayInfor extends React.Component {
+// constructor(props) {
+//   console.log(">>> call me constructor: 1");
+//   super(props);
+//   //babel compiler
+//   this.state = {
+//     isShowListUser: true,
+//   };
+// }
 
-  componentDidMount() {
-    console.log(">>> call me component did mount");
-    setTimeout(() => {
-      document.title = "Eric & Hoi Dan IT";
-    }, 3000);
-  }
+// componentDidMount() {
+//   console.log(">>> call me component did mount");
+//   setTimeout(() => {
+//     document.title = "Eric & Hoi Dan IT";
+//   }, 3000);
+// }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log(">>> call me component did update", this.props, prevProps);
-    if (this.props.listUsers !== prevProps.listUsers) {
-      if (this.props.listUsers.length === 5) {
-        alert("You have 5 users!");
-      }
-    }
-  }
+// componentDidUpdate(prevProps, prevState, snapshot) {
+//   console.log(">>> call me component did update", this.props, prevProps);
+//   if (this.props.listUsers !== prevProps.listUsers) {
+//     if (this.props.listUsers.length === 5) {
+//       alert("You have 5 users!");
+//     }
+//   }
+// }
 
-  handleShowHide = function () {
-    this.setState({ isShowListUser: !this.state.isShowListUser });
+// handleShowHide = function () {
+//   this.setState({ isShowListUser: !this.state.isShowListUser });
+// };
+// render() {
+//   console.log("call me render");
+//   // destructuring array/object
+//   const { listUsers } = this.props;
+//   // props => viết tắt của từ properties
+
+//   // component = template + logic của javascript
+//   return (
+//     <div className="display-infor-container">
+//       {true && (
+//         <>
+//           {listUsers.map((user) => {
+//             return (
+//               <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+//                 <div>
+//                   <div>My name is {user.name}</div>
+//                   <div>My age is {user.age}</div>
+//                 </div>
+//                 <div>
+//                   <button
+//                     onClick={() => this.props.handleDeleteUser(user.id)}
+//                   >
+//                     Delete
+//                   </button>
+//                 </div>
+//                 <hr></hr>
+//               </div>
+//             );
+//           })}
+//           {/* {<div>My name is {name}</div>
+//       <div>My age is {age}</div>
+//       <hr></hr>
+//       <div>My name is {name}</div>
+//       <div>My age is {age}</div>
+//       <hr></hr>
+//       <div>My name is {name}</div>
+//       <div>My age is {age}</div>} */}
+//         </>
+//       )}
+//     </div>
+//   );
+// }
+//}
+
+const DisplayInfor = (props) => {
+  const { listUsers } = props; // Đối với function component, không hề có từ khoá this
+
+  const [isShowHideListUser, setShowHideListUser] = useState(true);
+
+  const handleShowHideListUser = () => {
+    setShowHideListUser(!isShowHideListUser);
   };
-  render() {
-    console.log("call me render");
-    // destructuring array/object
-    const { listUsers } = this.props;
-    // props => viết tắt của từ properties
 
-    // component = template + logic của javascript
-    return (
-      <div className="display-infor-container">
-        {/* <img src={logo} /> */}
-        <div>
-          <span
-            onClick={() => {
-              this.handleShowHide();
-            }}
-          >
-            {this.state.isShowListUser ? "Hide list users" : "Show list users"}
-          </span>
-        </div>
-
-        {this.state.isShowListUser && (
-          <>
-            {listUsers.map((user) => {
-              return (
-                <div key={user.id} className={user.age > 18 ? "green" : "red"}>
-                  <div>
-                    <div>My name is {user.name}</div>
-                    <div>My age is {user.age}</div>
-                  </div>
-                  <div>
-                    <button
-                      onClick={() => this.props.handleDeleteUser(user.id)}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                  <hr></hr>
+  return (
+    <div className="display-infor-container">
+      <div>
+        <span onClick={() => handleShowHideListUser()}>
+          {isShowHideListUser === true ? "Hide list users" : "Show list users"}
+        </span>
+      </div>
+      {isShowHideListUser && (
+        <>
+          {listUsers.map((user) => {
+            return (
+              <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+                <div>
+                  <div>My name is {user.name}</div>
+                  <div>My age is {user.age}</div>
                 </div>
-              );
-            })}
-            {/* {<div>My name is {name}</div>
+                <div>
+                  <button onClick={() => props.handleDeleteUser(user.id)}>
+                    Delete
+                  </button>
+                </div>
+                <hr></hr>
+              </div>
+            );
+          })}
+          {/* {<div>My name is {name}</div>
         <div>My age is {age}</div>
         <hr></hr>
         <div>My name is {name}</div>
@@ -79,11 +117,10 @@ class DisplayInfor extends React.Component {
         <hr></hr>
         <div>My name is {name}</div>
         <div>My age is {age}</div>} */}
-          </>
-        )}
-      </div>
-    );
-  }
-}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default DisplayInfor;
